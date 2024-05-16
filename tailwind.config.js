305228e548d8daae5217e803f873e675d1f301fb
tgrsnts <tegarsantoso72@gmail.com>
2024-05-16 22:13:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'body': ['"Poppins"'],
+              },
+            colors: {
+                'first': '#CCA034',
+                'title': '#0A1343',
+                'text': '#5a5e72',
+                'body': '#f5f7ff',
+                'container': '#f0f2ff',
+                'border': '	#bfc4d9',
+            },
+        }
+    }
+  }
