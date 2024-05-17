0426d621aa0dd87f16b37242ab4bcf8b2af3b5ea
tgrsnts <tegarsantoso72@gmail.com>
2024-05-17 22:06:53 +0700

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            fontFamily: {
-                'body': ['"Poppins"'],
-              },
-            colors: {
-                'first': '#CCA034',
-                'title': '#0A1343',
-                'text': '#5a5e72',
-                'body': '#f5f7ff',
-                'container': '#f0f2ff',
-                'border': '	#bfc4d9',
-            },
-        }
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': ['"Poppins"'],
+      },
+      colors: {
+        'first': '#CCA034',
+        'title': '#0A1343',
+        'text': '#5a5e72',
+        'body': '#f5f7ff',
+        'container': '#f0f2ff',
+        'border': '	#bfc4d9',
+      },
+      backgroundImage: {
+        '14': "url('/images/14.jpg')",
+        '15': "url('/images/15.jpg')",
+        '17': "url('/images/17.jpg')",
+      }
     }
   }
+}
